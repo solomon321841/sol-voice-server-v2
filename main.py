f9cb1181ae2bc339d3c94917c9cfbd4b2ecc56c6
Solomon Roth <solomonroth@Solomons-MacBook-Air.local>
2025-11-23 14:30:46 -0500

    update main

--- main.py
+++ main.py
@@ -432,6 +432,6 @@ async def websocket_handler(ws: WebSocket):
 #  RUN
 # =====================================================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000"))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
 
