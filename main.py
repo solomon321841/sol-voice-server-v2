1e3c5aa53883b0a1cef41645fd821a9168549b38
Solomon Roth <solomonroth@Solomons-MacBook-Air.local>
2025-11-24 18:00:46 -0500

    update main

--- main.py
+++ main.py
@@ -233,7 +233,7 @@ async def websocket_handler(ws: WebSocket):
 
     dg_url = (
         "wss://api.deepgram.com/v1/listen"
-        "?model=nova-2&encoding=linear16&sample_rate=48000"
+        "?model=nova-2&encoding=linear16&sample_rate=48000&raw=true"
         "&punctuate=true&smart_format=true"
     )
 
